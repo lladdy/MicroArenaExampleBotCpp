91d94abe92d898ed661d803b6e77ea9e75b63230
lladdy <randallboland@bigpond.com>
2018-11-11 20:45:12 +1030

    Have bot attack across map.

--- src/ExampleBotMain.cpp
+++ src/ExampleBotMain.cpp
@@ -15,9 +15,36 @@ public:
 
     virtual void OnStep() final {
         // play game here...
+
+        attack();
     };
 
 private:
+
+    void attack() {
+        const sc2::ObservationInterface *observation = Observation();
+        sc2::Point2D playableArea = getPlayableArea(observation);
+        const sc2::Units units = observation->GetUnits(sc2::Unit::Alliance::Self);
+        sc2::Point2D target_pos;
+        for (const auto &unit : units) {
+            if (unit->orders.size() == 0) { // if they have no current orders, attack across map
+                Actions()->UnitCommand(
+                        unit,
+                        sc2::ABILITY_ID::ATTACK,
+                        getAcrossMapAttackPoint(unit, playableArea)
+                );
+            }
+        }
+    }
+
+    sc2::Point2D getPlayableArea(const sc2::ObservationInterface *observation) {
+        auto game_info_ = observation->GetGameInfo();
+        return game_info_.playable_max - game_info_.playable_min;
+    }
+
+    sc2::Point2D getAcrossMapAttackPoint(const sc2::Unit *unit, const sc2::Point2D playableArea) {
+        return playableArea - unit->pos;
+    }
 };
 
 int main(int argc, char *argv[]) {
